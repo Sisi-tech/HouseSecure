184c42b43df4da3ff18b02887cd4ba0a6759a322
Sisi Wang <sisiwang242@gmail.com>
2025-01-26 00:02:47 -0500

    add style to input

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -37,14 +37,14 @@ const Login = () => {
             <div className='flex flex-col items-center justify-center w-full mt-20 sm:mt-28 md:mt-32 lg:mt-36'>
                 <div className='w-full max-w-md p-6 bg-white shadow-md rounded-lg'>
                     <form onSubmit={handleSubmit} className="space-y-10">
-                        <legend className='text-2xl font-bold text-center'>Login</legend>
+                        <legend className='text-2xl font-bold text-center text-gray-800'>Login</legend>
                         <div className='flex flex-col space-y-10'>
                             <input 
                                 type="email" 
                                 value={email}
                                 onChange={handleEmailChange}
                                 placeholder='Email' 
-                                className='w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500' 
+                                className='w-full px-4 py-2 border border-gray-800 text-gray-800 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500' 
                                 required
                                 pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,)$"
                             />
@@ -53,7 +53,7 @@ const Login = () => {
                                 <input 
                                     type={showPassword ? "text" : "password"}
                                     placeholder="Password" 
-                                    className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" 
+                                    className="w-full px-4 py-2 border border-gray-800 text-gray-800 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" 
                                     value={password}
                                     onChange={handlePasswordChange}
                                 />
@@ -71,7 +71,7 @@ const Login = () => {
                                 </button>
                             </div>
                             <button type="submit" className="w-full py-2 bg-blue-500 text-white rounded-md hover:bg-blue-700 transition">Sign In</button>
-                            <div className='text-sm text-center'>
+                            <div className='text-sm text-center text-gray-800'>
                                 <p>
                                     Don't have an account?{" "}
                                     <Link to="/signUp" className='text-blue-500 hover:underline'>Sign Up</Link>
