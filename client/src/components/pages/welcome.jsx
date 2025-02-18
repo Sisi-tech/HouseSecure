bb483963294ccf6e8bc537eb237e0fbdc794ed90
Sisi Wang <sisiwang242@gmail.com>
2025-02-18 16:16:53 -0500

    add gif

--- client/src/components/pages/welcome.jsx
+++ client/src/components/pages/welcome.jsx
@@ -29,14 +29,23 @@ const Welcome = () => {
                 className='flex justify-center w-full min-h-screen bg-no-repeat bg-contain bg-right'
                 style={{ backgroundImage: `url(${backgroundImage})` }}
             >
-                <div className="w-4/5 md:w-3/5 h-auto md:h-3/5 flex flex-col items-center gap-6 mt-20 p-10 rounded-lg shadow-lg">
-                    <h1 className="text-xl">Welcome to <span className="font-semibold text-blue-600">HomeSecure</span> {user ? `${user.firstName} ${user.lastName}` : ""}!</h1>
-                    <p className="text-lg">
-                        We're glad to have you here. Let's work together to make our homes protected, safe, and full of happiness.
-                    </p>
-                    <p className="text-lg">
-                        Your security is our priority. Explore your coverages, manage your policies, and stay informed with HomeSecure.
-                    </p>
+                <div className="w-4/5 md:w-3/5 h-auto flex flex-col gap-2 p-6 rounded-lg shadow-lg">
+                    <div className="flex justify-center">
+                        <img
+                            src="https://media.giphy.com/media/Wj7lNjMNDxSmc/giphy.gif"
+                            alt="Excited Dog GIF"
+                            className="w-40 h-auto"
+                        />
+                    </div>
+                    <div className="text-center text-md">
+                        <h1 className="font-semibold pb-2">Welcome to <span className="semibold text-blue-600">HomeSecure</span> {user ? `${user.firstName} ${user.lastName}` : ""}!</h1>
+                        <p>
+                            We're glad to have you here. Let's work together to make our homes protected, safe, and full of happiness.
+                        </p>
+                        <p>
+                            Your security is our priority. Explore your coverages, manage your policies, and stay informed with HomeSecure.
+                        </p>
+                    </div>
                 </div>
             </div>
             <BackToTop />
